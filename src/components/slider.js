b223628786fe7f39e723c019bfc109d1bd653829
david <mv12013@ues.edu.sv>
2024-06-02 10:04:40 -0600

    configurar modo responsive

--- src/components/slider.js
+++ src/components/slider.js
@@ -129,10 +129,10 @@ export function initializeSwiper(slidesData) {
 		loop: true,
 		slidesPerView: 1.5,
 		centeredSlides: true,
-		autoplay: {
-			delay: 3000,
-			disableOnInteraction: false,
-		},
+		// autoplay: {
+		// 	delay: 3000,
+		// 	disableOnInteraction: false,
+		// },
 		pagination: {
 			el: ".swiper-pagination",
 			clickable: true,
