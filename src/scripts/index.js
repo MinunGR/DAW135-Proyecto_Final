ec10f3a7961c0ef987e2e88bde0ee991e9e6d298
Fernando Barraza <fernando.jba.2000@gmail.com>
2024-06-22 23:40:32 -0600

    22/06/2024- Ajustes a js de live chat
    - Se paso a un documento js aparte el script del chat en vivo
    - Se removieron scripts de sobra

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,6 @@
 import { initializeSwiper, slidesData } from "../components/slider.js";
 import "../components/scroll.js";
-import "../components/email.js"
+import "../components/email.js";
+import "../components/chat.js";
 
 initializeSwiper(slidesData);
